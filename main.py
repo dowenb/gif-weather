42a5773114f3dbf75740374e89ca2e8cc5f5b049
Ben Dowen <ben@dowen.me.uk>
2020-07-18 01:45:59 +0100

    add CORS for local dev; improved error handling.

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 import requests
+
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/{location}")
@@ -16,15 +33,18 @@ def weather_at_location(location_name):
         location=location_name)
     response = requests.get(request_url)
     response_json = response.json()
-    woeid = response_json[0]["woeid"]
-    # Get the weather description
-    request_url = 'https://www.metaweather.com/api/location/{woeid}/'.format(
-        woeid=woeid)
-    response = requests.get(request_url)
-    response_json = response.json()
-    location_weather = response_json["consolidated_weather"][0]["weather_state_name"]
+    if response_json == []:
+        raise HTTPException(status_code=404, detail="Weather location not found.")
+    else:    
+        woeid = response_json[0]["woeid"]
+        # Get the weather description
+        request_url = 'https://www.metaweather.com/api/location/{woeid}/'.format(
+            woeid=woeid)
 
+    response = requests.get(request_url)
     if response.ok:
+        response_json = response.json()
+        location_weather = response_json["consolidated_weather"][0]["weather_state_name"]
         return {"weather": location_weather}
     else:
         raise HTTPException(
@@ -32,14 +52,12 @@ def weather_at_location(location_name):
 
 
 def gif_for_weather(weather):
-    request_url = 'https://api.giphy.com/v1/gifs/search?api_key=QloqXubxa4z9GgZQ6JqwbAeXbscVzz97&q={weather}&limit=1&offset=0&rating=g&lang=en'.format(
+    request_url = 'https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q={weather}&limit=1&offset=0&rating=g&lang=en'.format(
         weather=weather)
     response = requests.get(request_url)
-    response_json = response.json()
-    gif_url = response_json["data"][0]["url"]
-
     if response.ok:
-        return {"url": gif_url}
+        response_json = response.json()
+        return response_json
     else:
         raise HTTPException(
             status_code=404, detail="GIF not found.")
