bd582a1661566db1767d8c78286ccf67a275b758
Ben Dowen <ben@dowen.me.uk>
2020-07-19 15:08:37 +0100

    fix circle test

--- test_main.py
+++ test_main.py
@@ -8,4 +8,3 @@ client = TestClient(app)
 def test_gif_weather():
     response = client.get("/london")
     assert response.status_code == 200
-    assert response.json() == {"url":"https://giphy.com/gifs/vhs-positive-vhspositive-3o6EhOYMhOTANYgHMk"}
